597ae64bc6cc7edaa39230d767632cebe0f51187
aleks_pv <apanov08@gmail.com>
2022-06-03 10:44:15 +0400

    refactor: add break line

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import fs from 'node:fs/promises'
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const srcFile = path.join(__dirname, 'files', 'fresh.txt');
