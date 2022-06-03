f753a020c5eba2b769e22a814510e317dd11e4b0
aleks_pv <apanov08@gmail.com>
2022-06-03 10:43:04 +0400

    feat: done fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,35 @@
-export const copy = async () => {
-    // Write your code here 
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs/promises'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathDir = path.join(__dirname, 'files');
+const pathCopyDir = path.join(__dirname, 'files_copy');
+
+export const copy = async (src, options, srcCopyDir) => {
+    await fs.mkdir(srcCopyDir);
+			copyFile(src, options).catch(err => {
+				if(err) {
+					console.error('FS operation failed');
+				}
+			})
+			
 };
+const copyFile = async (src, options) => {
+  const readDir = await fs.readdir(src, options);
+	readDir.map(file => {
+		let srcFile = path.join(pathDir, `${file.name}`);
+				let destFile = path.join(pathCopyDir, `${file.name}`);
+			if(file.isFile()) {				
+				fs.copyFile(srcFile, destFile);
+			} else {
+				copyFile(srcFile, options);
+			}
+		})
+}
+copy(pathDir, {withFileTypes: true}, pathCopyDir).catch(err => {
+	if(err) {
+		console.error('FS operation failed');
+	}
+});
