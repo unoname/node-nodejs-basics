e1e21cdb1ad744e87cc188285d8cd76ef4b64b1b
aleks_pv <apanov08@gmail.com>
2022-06-03 11:18:57 +0400

    feat: done fs\list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
-export const list = async () => {
-    // Write your code here 
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs/promises'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathDir = path.join(__dirname, 'files');
+
+export const list = async (src, options) => {    
+		const readDir = await fs.readdir(src, options);
+		const arrNameFile = [];
+	readDir.map(file => {
+		arrNameFile.push(file.name);
+		});
+				console.log(arrNameFile);
 };
+
+list(pathDir, {withFileTypes: true}).catch(err => {
+	if(err) {
+		console.error('FS operation failed');
+	}
+});
