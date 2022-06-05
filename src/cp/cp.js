68d155be3ff489077d09cd81727edb4aaf7fe9bb
aleks_pv <apanov08@gmail.com>
2022-06-05 23:52:28 +0400

    feat: done cp\cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,16 @@
-export const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { stdin as readable, stdout as writable } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const args = process.argv.slice(2);
+const srcChild = path.join(__dirname, 'files', 'script.js');
+
+export const spawnChildProcess = async (args, src) => {
+    const childProcess = fork(src, args);
+		childProcess.send(readable);
 };
+
+spawnChildProcess(args, srcChild)
