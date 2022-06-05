78aa347cd67c535223da90b0098c9218921d1d24
aleks_pv <apanov08@gmail.com>
2022-06-05 17:38:48 +0400

    feat: done hash\calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const { createHash } = await import('crypto');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+
+
+export const calculateHash = async (src) => {
+    const hash = createHash('sha256');
+    const input = createReadStream(src);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
+
+calculateHash(pathFile)
