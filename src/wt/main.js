f2b072906a4d5189c1d661bbbe76fd2b601cb80e
aleks_pv <apanov08@gmail.com>
2022-06-15 17:43:07 +0400

    fix: change src\wt\main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -7,27 +7,22 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const srcFile = path.join(__dirname, 'worker.js');
-const amountCPU = cpus().length + 10;
+const numberCpus = cpus();
 
-export const performCalculations = async () => {    
-			let worker_1 = new Worker(srcFile, {workerData: 10});
-		let worker_2 = new Worker(srcFile, {workerData: 11});
-		let worker_3 = new Worker(srcFile, {workerData: 12});
-		let worker_4 = new Worker(srcFile, {workerData: 13});
-		let result = [];
-		
-			worker_1.on('message', (msg) => {
-				result.push(msg);				
+export const performCalculations = async () => {   
+	 let results = [];
+	for(let i = 10; i < numberCpus.length + 10; i++) {
+		results.push(await new Promise((res) => {
+		let worker = new Worker(srcFile, {workerData: i});	
+			worker.on('message', (msg) => {
+				res({status: 'resolved', data: msg});				
 			})
-			worker_2.on('message', (msg) => {
-				result.push(msg);				
-			})
-			worker_3.on('message', (msg) => {
-				result.push(msg);				
-			})
-			worker_4.on('message', (msg) => {
-				result.push(msg);
-				console.log(result.sort());
-			})		
+			worker.on('error', () => {				
+				res({status: 'rejected', data: null})		        
+			 })
+		 })
+		)	
+	}
+	console.log(results);
 };
 performCalculations()
